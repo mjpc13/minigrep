3c0a77fc460dffa1caf6b3b0a5ca619dde5a4cd0
mjpc13 <mjpc13@protonmail.com>
2022-06-05 19:15:16 +0100

    Remove the prints

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,5 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("Text in file:\n{}", contents);
-
     Ok(())
 }
